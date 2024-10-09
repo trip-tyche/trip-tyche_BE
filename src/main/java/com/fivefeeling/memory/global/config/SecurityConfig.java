592e1ba7c1e1755ea5882443d880097992d007e5
marista-dev <sa21788@naver.com>
2024-10-10 03:05:25 +0900

    up

--- src/main/java/com/fivefeeling/memory/global/config/SecurityConfig.java
+++ src/main/java/com/fivefeeling/memory/global/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://ec2-43-203-13-252.ap-northeast-2.compute.amazonaws.com"));
+    configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://ec2-43-203-13-252.ap-northeast-2.compute.amazonaws.com", "https://5feeling-fe.vercel.app"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
