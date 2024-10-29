245072f0a208760f299854b6ef0cfa68792d37a8
marista-dev <sa21788@naver.com>
2024-10-29 23:01:44 +0900

    #61-AsyncConfig 수정

--- src/main/java/com/fivefeeling/memory/global/config/AsyncConfig.java
+++ src/main/java/com/fivefeeling/memory/global/config/AsyncConfig.java
@@ -11,9 +11,10 @@ public class AsyncConfig {
   @Bean(name = "fileProcessingTaskExecutor")
   public Executor fileProcessingTaskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(20);  // 기본 유지 스레드 수
-    executor.setMaxPoolSize(25);   // 최대 스레드 수
-    executor.setQueueCapacity(50); // 대기열 크기
+    executor.setCorePoolSize(2);  // 기본 유지 스레드 수
+    executor.setMaxPoolSize(4);   // 최대 스레드 수
+    executor.setQueueCapacity(20); // 대기열 크기
+    executor.setKeepAliveSeconds(30); // 유휴 스레드 유지 시간
     executor.setThreadNamePrefix("파일업로드-");
     executor.initialize();
     return executor;
