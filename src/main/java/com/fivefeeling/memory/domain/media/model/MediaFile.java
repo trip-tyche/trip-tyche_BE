3b75f1d4d732c990a810cfe16dcd938e564efe31
marista-dev <sa21788@naver.com>
2024-10-31 23:08:50 +0900

    #63 refac: MediaFile 엔티티 인덱스 추가
    MediaFile 엔티티에 tripId와 pinPointId를 위한 인덱스 추가
    - tripId에 대한 인덱스 추가
    - pinPointId에 대한 인덱스 추가

--- src/main/java/com/fivefeeling/memory/domain/media/model/MediaFile.java
+++ src/main/java/com/fivefeeling/memory/domain/media/model/MediaFile.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,7 +23,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "MediaFile")
+@Table(name = "MediaFile", indexes = {
+    @Index(name = "idx_media_file_trip_id", columnList = "tripId"),
+    @Index(name = "idx_media_file_pin_point_id", columnList = "pinPointId")
+})
 public class MediaFile {
 
   @Id
