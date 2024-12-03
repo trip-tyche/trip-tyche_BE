176fad32d192eb5ea4aaab4c24b4c4fe854aff49
marista-dev <sa21788@naver.com>
2024-12-03 21:01:43 +0900

    #73 refac: PresignedURL 수정

--- src/main/java/com/fivefeeling/memory/domain/media/controller/PresignedURLController.java
+++ src/main/java/com/fivefeeling/memory/domain/media/controller/PresignedURLController.java
@@ -1,35 +1,43 @@
 package com.fivefeeling.memory.domain.media.controller;
 
+import com.fivefeeling.memory.domain.media.dto.FilePresignedRequest;
+import com.fivefeeling.memory.domain.media.dto.FilePresignedResponse;
+import com.fivefeeling.memory.domain.media.dto.FilePresignedResponse.PresignedUrlDetail;
+import com.fivefeeling.memory.global.common.RestResponse;
 import com.fivefeeling.memory.global.s3.PresignedURLService;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/trips")
 public class PresignedURLController {
 
   private final PresignedURLService presignedURLService;
 
-  @PostMapping("/api/trips/{tripId}/presigned-url")
-  public ResponseEntity<Map<String, String>> generatePresignedUrl(
-      @RequestParam String fileName,
-      @RequestParam String fileType,
-      @RequestParam Long tripId) {
+  @PostMapping("{tripId}/presigned-url")
+  public RestResponse<FilePresignedResponse> generatePresignedUrl(
+      @PathVariable Long tripId,
+      @RequestBody FilePresignedRequest request) {
 
-    String fileKey = "upload/" + tripId + "/" + fileName;
+    List<PresignedUrlDetail> presignedUrls = request.files().stream()
+        .map(file -> {
+          String fileKey = "upload/" + tripId + "/" + file.fileName();
+          String presignedPutUrl = presignedURLService.generatePresignedPutUrl(
+              fileKey, file.fileType(), Duration.ofMinutes(10)
+          );
 
-    String presignedPutUrl = presignedURLService.generatePresignedPutUrl(fileKey, fileType, Duration.ofMinutes(10));
+          return new FilePresignedResponse.PresignedUrlDetail(fileKey, presignedPutUrl);
+        })
+        .collect(Collectors.toList());
 
-    Map<String, String> response = new HashMap<>();
-    response.put("presignedPutUrl", presignedPutUrl);
-    response.put("fileKey", fileKey);
-
-    return ResponseEntity.ok(response);
+    return RestResponse.success(new FilePresignedResponse(presignedUrls));
   }
 }
