5380d0168a8dbd57ff3c2a8447a36c04e255c3d7
marista-dev <sa21788@naver.com>
2025-01-15 22:45:27 +0900

    #84 feat: 의존성 추가 및 OpenAPI 설정 업데이트
    의존성 추가 및 버전 명시
    - Prometheus 레지스트리 의존성에 버전 추가
    - Hibernate 공통 주석 의존성 추가
    
    OpenAPI 설정 업데이트
    - 공유 관련 API 태그 추가

--- src/main/java/com/fivefeeling/memory/global/config/OpenAPIConfig.java
+++ src/main/java/com/fivefeeling/memory/global/config/OpenAPIConfig.java
@@ -16,24 +16,25 @@ public class OpenAPIConfig {
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
-        .info(new Info()
-            .title("Trip Tyche API")
-            .version("1.0"))
-        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-        .components(new Components()
-            .addSecuritySchemes("bearerAuth",
-                new SecurityScheme()
-                    .name("bearerAuth")
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT")))
-        .tags(List.of(
-            new Tag().name("1. 메인 페이지 API"),
-            new Tag().name("2. 여행관리 페이지 API"),
-            new Tag().name("3. 여행등록 페이지 API"),
-            new Tag().name("4. Map 페이지 API"),
-            new Tag().name("5. 날짜별 페이지 API"),
-            new Tag().name("6. 위치정보 없는 수정 페이지 API"),
-            new Tag().name("사용하지 않는 API")));
+            .info(new Info()
+                    .title("Trip Tyche API")
+                    .version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                    .addSecuritySchemes("bearerAuth",
+                            new SecurityScheme()
+                                    .name("bearerAuth")
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")))
+            .tags(List.of(
+                    new Tag().name("1. 메인 페이지 API"),
+                    new Tag().name("2. 여행관리 페이지 API"),
+                    new Tag().name("3. 여행등록 페이지 API"),
+                    new Tag().name("4. Map 페이지 API"),
+                    new Tag().name("5. 날짜별 페이지 API"),
+                    new Tag().name("6. 위치정보 없는 수정 페이지 API"),
+                    new Tag().name("7. 공유 관련 API"),
+                    new Tag().name("사용하지 않는 API")));
   }
 }
