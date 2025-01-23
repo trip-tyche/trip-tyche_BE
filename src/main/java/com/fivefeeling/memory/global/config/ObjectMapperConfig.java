6f72da2214484e1bb6a64c012d5d15d2acdaa50e
marista-dev <sa21788@naver.com>
2025-01-23 09:55:36 +0900

    refac

--- src/main/java/com/fivefeeling/memory/global/config/ObjectMapperConfig.java
+++ src/main/java/com/fivefeeling/memory/global/config/ObjectMapperConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.fivefeeling.memory.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,3 +20,4 @@ public class ObjectMapperConfig {
     return objectMapper;
   }
 }
+*/
