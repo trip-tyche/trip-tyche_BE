53050e62a9d59198a400e82e2b4b86c33c849673
marista-dev <sa21788@naver.com>
2025-01-23 23:12:02 +0900

    #91 refactor: ShareRepository 메서드 수정
    ShareRepository의 메서드 수정
    - findByRecipientId 메서드 제거
    - findAllByRecipientId 메서드로 대체하여 모든 결과 반환

--- src/main/java/com/fivefeeling/memory/domain/share/repository/ShareRepository.java
+++ src/main/java/com/fivefeeling/memory/domain/share/repository/ShareRepository.java
@@ -3,7 +3,6 @@ package com.fivefeeling.memory.domain.share.repository;
 import com.fivefeeling.memory.domain.share.model.Share;
 import com.fivefeeling.memory.domain.trip.model.Trip;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface ShareRepository extends JpaRepository<Share, Long> {
 
   boolean existsByTripAndRecipientId(Trip trip, Long recipientId);
-
-  Optional<Share> findByRecipientId(Long recipientId);
-
+  
   List<Share> findAllByRecipientId(Long recipientId);
 }
