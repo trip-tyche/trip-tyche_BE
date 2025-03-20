a75e229fccc6ac5daa99492e270e398855b075c5
marista-dev <sa21788@naver.com>
2025-03-20 23:17:53 +0900

    #117 refactor: 사용자 닉네임 업데이트 기능 개선
    닉네임 업데이트 API 수정 및 요청 DTO 추가
    - HTTP 메서드 변경: POST에서 PATCH로
    - URL 경로 변경 및 리팩토링
    - UpdateNickNameRequest DTO 추가하여 요청 데이터 구조화

--- src/main/java/com/fivefeeling/memory/domain/user/controller/UserProfileController.java
+++ src/main/java/com/fivefeeling/memory/domain/user/controller/UserProfileController.java
@@ -1,15 +1,15 @@
 package com.fivefeeling.memory.domain.user.controller;
 
+import com.fivefeeling.memory.domain.user.dto.UpdateNickNameRequest;
 import com.fivefeeling.memory.domain.user.repository.UserRepository;
 import com.fivefeeling.memory.domain.user.service.UserService;
 import com.fivefeeling.memory.global.common.RestResponse;
 import com.fivefeeling.memory.global.util.JwtTokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "1. 메인 페이지 API")
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/v1/users")
 @RequiredArgsConstructor
 public class UserProfileController {
 
@@ -27,22 +27,25 @@ public class UserProfileController {
   private final JwtTokenProvider jwtTokenProvider;
   private final UserRepository userRepository;
 
-  @Operation(summary = "사용자 닉네임 등록", description = "<a href='https://www.notion.so/maristadev/865a54c429e649fe8646be7da6954a4a?pvs=4' target='_blank'>API 명세서</a>")
-  @PostMapping("/updateUserNickName")
+  @Operation(summary = "사용자 닉네임 업데이트", description = "<a href='https://www.notion"
+          + ".so/maristadev/865a54c429e649fe8646be7da6954a4a?pvs=4' target='_blank'>API 명세서</a>")
+  @PatchMapping("/me")
   @ResponseBody
-  public RestResponse<String> updateNickName(@RequestHeader("Authorization") String authorizationHeader,
-      @Parameter(description = "사용자 닉네임", required = true) @RequestBody String userNickName) {
+  public RestResponse<String> updateNickName(
+          @RequestHeader("Authorization") String authorizationHeader,
+          @RequestBody UpdateNickNameRequest updateRequest) {
 
     // JWT 토큰에서 이메일을 추출
     String token = authorizationHeader.substring(7);
     String userEmail = jwtTokenProvider.getUserEmailFromToken(token);
 
     // 닉네임 업데이트 서비스 호출
-    userService.updateUserNickNameByEmail(userEmail, userNickName);
+    userService.updateUserNickNameByEmail(userEmail, updateRequest.nickname());
     return RestResponse.success("닉네임이 성공적으로 등록되었습니다.");
   }
 
-  @Operation(summary = "닉네임 중복 확인", description = "<a href='https://www.notion.so/maristadev/17766958e5b380afad4ef68810296b0b?pvs=4' target='_blank'>API 명세서</a>")
+  @Operation(summary = "닉네임 중복 확인", description = "<a href='https://www.notion"
+          + ".so/maristadev/17766958e5b380afad4ef68810296b0b?pvs=4' target='_blank'>API 명세서</a>")
   @GetMapping("/check-nickname")
   public RestResponse<String> checkNicknameAvailability(@RequestParam String userNickName) {
     userService.validateAndCheckNickname(userNickName);
