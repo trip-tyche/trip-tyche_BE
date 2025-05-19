e84bf2177e6a2b74e34b2a1ba6b7e0b5530b57df
marista-dev <sa21788@naver.com>
2025-05-20 02:08:14 +0900

    feat: 알림 상세 조회 기능 추가 및 API 경로 수정
    알림 상세 조회 기능 추가 및 API 경로 수정
    - 알림 목록 조회 및 상태 변경 API 경로를 개선
    - 알림 상세 조회 메서드와 DTO 추가
    - 기존 기능의 가독성 향상 및 유지보수 용이성 개선

--- src/main/java/com/fivefeeling/memory/domain/notification/controller/NotificationController.java
+++ src/main/java/com/fivefeeling/memory/domain/notification/controller/NotificationController.java
@@ -1,5 +1,6 @@
 package com.fivefeeling.memory.domain.notification.controller;
 
+import com.fivefeeling.memory.domain.notification.dto.NotificationDetailDTO;
 import com.fivefeeling.memory.domain.notification.dto.NotificationResponseDTO;
 import com.fivefeeling.memory.domain.notification.service.NotificationService;
 import com.fivefeeling.memory.global.common.RestResponse;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/notifications")
+@RequestMapping("/v1")
 @RequiredArgsConstructor
 @Tag(name = "9. 알림 관련 API")
 public class NotificationController {
@@ -24,7 +25,7 @@ public class NotificationController {
 
   @Operation(summary = "알림 목록 조회", description = "<a href='https://www.notion"
           + ".so/maristadev/17766958e5b3803690defb16a09d8c88?pvs=4' target='_blank'>API 명세서</a>")
-  @GetMapping("/{userId}")
+  @GetMapping("users/{userId}/notifications")
   public RestResponse<List<NotificationResponseDTO>> getUnreadNotifications(
           @PathVariable Long userId
   ) {
@@ -33,13 +34,21 @@ public class NotificationController {
 
   @Operation(summary = "알림 상태 변경(UNREAD -> READ)", description = "<a href='https://www.notion"
           + ".so/maristadev/18566958e5b3801ea257fcfbe2d9e2e0?pvs=4' target='_blank'>API 명세서</a>")
-  @PatchMapping("/{notificationId}")
+  @PatchMapping("/notifications/{notificationId}")
   public RestResponse<String> markAsRead(
           @PathVariable Long notificationId
   ) {
     notificationService.markAsRead(notificationId);
 
     return RestResponse.success("알림 상태 변경(UNREAD -> READ) 완료");
+  }
+
+  @Operation(summary = "알림 상세 조회", description = "<a href='' target='_blank'>API 명세서</a>")
+  @GetMapping("/notifications/{notificationId}")
+  public RestResponse<NotificationDetailDTO> getNotificationDetail(
+          @PathVariable Long notificationId
+  ) {
+    return RestResponse.success(notificationService.getNotificationDetail(notificationId));
   }
 
   @Operation(summary = "알림 상태 변경 (READ -> DELETE)", description = "<a href='https://www.notion"
