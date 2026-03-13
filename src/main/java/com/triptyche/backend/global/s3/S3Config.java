f9303d8310432dc70ebae797653b4627aaf77fad
marista-dev <sa21788@naver.com>
2026-03-13 14:38:33 +0900

    fix: OCI Object Storage 호환을 위한 S3 path-style 접근 설정 추가
    - S3Client, S3Presigner에 path-style 접근 활성화
    - OCI S3 호환 API 사용 시 필수 설정

--- src/main/java/com/triptyche/backend/global/s3/S3Config.java
+++ src/main/java/com/triptyche/backend/global/s3/S3Config.java
@@ -1,5 +1,6 @@
 package com.triptyche.backend.global.s3;
 
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,8 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-
-import java.net.URI;
 
 @Configuration
 public class S3Config {
@@ -34,10 +34,11 @@ public class S3Config {
     AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
 
     return S3Client.builder()
-        .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
-        .region(Region.of(region))
-        .endpointOverride(URI.create(endpoint))
-        .build();
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+            .region(Region.of(region))
+            .endpointOverride(URI.create(endpoint))
+            .forcePathStyle(true)
+            .build();
   }
 
   @Bean
@@ -45,10 +46,11 @@ public class S3Config {
     AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
 
     return S3Presigner.builder()
-        .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
-        .region(Region.of(region))
-        .endpointOverride(URI.create(endpoint))
-        .build();
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+            .region(Region.of(region))
+            .endpointOverride(URI.create(endpoint))
+            .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+            .build();
   }
 
   @Bean
