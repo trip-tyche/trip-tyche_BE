8a4d2d16064bd59c0f88f4413c3637430dc6c824
marista-dev <sa21788@naver.com>
2026-03-23 21:45:01 +0900

    fix: 요청 유효성 검증 누락 및 검증 실패 오류 응답 수정
    - 여행 정보 수정 요청에 유효성 검증 적용
    - 유효성 검증 실패 시 500 대신 400으로 응답하도록 수정

--- src/main/java/com/triptyche/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/triptyche/backend/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.triptyche.backend.global.exception;
 import com.triptyche.backend.global.common.RestResponse;
 import com.triptyche.backend.global.common.ResultCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,12 @@ public class GlobalExceptionHandler {
     ResultCode resultCode = ex.getResultCode();
     RestResponse<Void> response = RestResponse.error(resultCode);
     return new ResponseEntity<>(response, resultCode.getHttpStatus());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<RestResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
+    RestResponse<Void> response = RestResponse.error(ResultCode.INVALID_REQUEST);
+    return new ResponseEntity<>(response, ResultCode.INVALID_REQUEST.getHttpStatus());
   }
 
   // 기타 예외 처리
